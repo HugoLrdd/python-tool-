7fefb8fcaafe974fa7810151e56b4d716b333d75
HugoLrdd <hugoloridn@gmail.com>
2026-05-23 15:57:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ elif choix == "🔗 Générateur de QR Code":
         qr = qrcode.QRCode(version=1, box_size=10, border=4)
         qr.add_data(url)
         qr.make(fit=True)
-        img = qr.make_image(fill_color="black", back_color="white")
+        img = qr.make_image(fill_color="blue", back_color="red ")
         buf = io.BytesIO()
         img.save(buf, format="PNG")
         st.image(buf.getvalue(), width=250)
